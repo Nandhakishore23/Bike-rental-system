055865034b897d81706afe14a1b9b9d4d101abc6
Nandhakishore A <131781031+Nandhakishore23@users.noreply.github.com>
2024-11-26 23:12:30 +0530

    Delete server directory

--- server/clientSideModules/getdata.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const authenticate = require("../middelware/authenticate");
-
-//getting user complete data
-module.exports = router.get('/getdata', authenticate, (req, res) =>{
-    res.send(req.rootUser);
-});
